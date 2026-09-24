0ced95010500a2271f1ee219259a5769497c8f03
Diamondluxe <hehediamondluxe@gmail.com>
2026-09-24 22:27:33 +0500

    React folder done

---
+++ React/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+    <div id='dad'>
+      <h1>Hello, React!</h1>
+      <h3>Welcome to my React application.</h3>
+    </div>
+    <div id='mom'>
+      <h1>Hello, React</h1>
+   </div>
+   </>
+  )
+}
+
+export default App
+
