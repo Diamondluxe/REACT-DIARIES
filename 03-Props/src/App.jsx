e237c0dc57fcf828e299d8d74bbf1e1e3d25ec6b
Diamondluxe <hehediamondluxe@gmail.com>
2026-09-25 19:14:14 +0500

    Style component created

--- 03-Props/src/App.jsx
+++ 03-Props/src/App.jsx
@@ -1,4 +1,4 @@
-import Card from './Components/Card'
+import Card from './Components/Card/Card.jsx'
 
 const App = () => {
 
--- 03-Props/src/Components/Card/Card.jsx
+++ 03-Props/src/Components/Card/Card.jsx
@@ -1,12 +1,14 @@
+import styles from './Card.module.css'
+
 
 const Card = (props) => {
 
     // console.log(props.user) // Accessing the user prop passed from the parent component
 
   return (
-    <div className="card">
+    <div className={styles.card}>
         <img src={props.img} alt="Image" />
-        <h1>{props.user}</h1>
+        <h1 >{props.user}</h1>
         <p>This is a simple component using props as age: {props.age}</p>
         <button>View {props.user}'s Profile</button>
     </div>
